492164c9290edf6d96a587e4ce903c4bcb836aec
averageBurger <97335001+averageBurger@users.noreply.github.com>
2022-01-09 19:54:18 +0000

    oof
    persistence

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,8 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public static class MainManager
 {
-    public static int coins = 0;
+    public static int coins = 9;
+    public static List<Color> colourArray;
+
+    static void Awake()
+    {
+        colourArray.Add(Color.white);
+        Load();
+    }
+
+    class SaveData
+    {
+        public int coins;
+        public List<Color> colourArray;
+    }
+
+    public static void Save()
+    {
+        var data = new SaveData();
+        data.coins = coins;
+        data.colourArray = colourArray;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public static void Load()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(path);
+
+            coins = data.coins;
+            colourArray = data.colourArray;
+        }
+    }
 }
---
+++ Assets/Scripts/ShopUIManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ShopUIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI coinText;
+
+    private void Update()
+    {
+        coinText.text = "Coins: " + MainManager.coins;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+   public void Blue()
+   {
+        PickColour(9, Color.blue);
+   }
+
+    public void Red()
+    {
+        PickColour(14, Color.red);
+    }
+
+    public void Yellow()
+    {
+        PickColour(19, Color.yellow);
+    }
+    public void Green()
+    {
+        PickColour(24, Color.green);
+    }
+
+    public void Purple()
+    {
+        PickColour(29, Color.magenta);
+    }
+
+    public void PickColour(int price, Color colour)
+    {
+        if (MainManager.coins > price && !MainManager.colourArray.Contains(colour))
+        {
+            MainManager.colourArray.Add(colour);
+            MainManager.coins -= (price + 1);
+        }
+    }
+}
